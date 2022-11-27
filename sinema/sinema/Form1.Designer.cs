aa99d784e38cb91347f1521f1cf9da2076c60fda
seevdey <svdeyilmaz@gmail.com>
2022-11-27 10:46:07 +0300

    24.11

---
+++ sinema/sinema/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace sinema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.biletbtn = new System.Windows.Forms.Button();
+            this.listelebtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(65, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kat : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(48, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Salon : ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C"});
+            this.comboBox1.Location = new System.Drawing.Point(141, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.comboBox2.Location = new System.Drawing.Point(141, 117);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(381, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Koltuklar ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(337, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.DoubleClick += new System.EventHandler(this.textBox1_DoubleClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(381, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(32, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            this.textBox2.DoubleClick += new System.EventHandler(this.textBox2_DoubleClick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(425, 72);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(30, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            this.textBox3.DoubleClick += new System.EventHandler(this.textBox3_DoubleClick);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(467, 72);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(31, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Click += new System.EventHandler(this.textBox4_Click);
+            this.textBox4.DoubleClick += new System.EventHandler(this.textBox4_DoubleClick);
+            // 
+            // biletbtn
+            // 
+            this.biletbtn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.biletbtn.FlatAppearance.BorderSize = 3;
+            this.biletbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.biletbtn.Location = new System.Drawing.Point(337, 117);
+            this.biletbtn.Name = "biletbtn";
+            this.biletbtn.Size = new System.Drawing.Size(161, 29);
+            this.biletbtn.TabIndex = 7;
+            this.biletbtn.Text = "Bilet Al";
+            this.biletbtn.UseVisualStyleBackColor = true;
+            this.biletbtn.Click += new System.EventHandler(this.biletbtn_Click);
+            // 
+            // listelebtn
+            // 
+            this.listelebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listelebtn.Location = new System.Drawing.Point(337, 152);
+            this.listelebtn.Name = "listelebtn";
+            this.listelebtn.Size = new System.Drawing.Size(161, 30);
+            this.listelebtn.TabIndex = 8;
+            this.listelebtn.Text = "Koltukları Listele";
+            this.listelebtn.UseVisualStyleBackColor = true;
+            this.listelebtn.Click += new System.EventHandler(this.listelebtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(52, 214);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(446, 199);
+            this.listBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(632, 484);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listelebtn);
+            this.Controls.Add(this.biletbtn);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sinema Uygulaması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button biletbtn;
+        private System.Windows.Forms.Button listelebtn;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ sinema/sinema/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinema
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[,,] sinema = new string[3, 2, 4]; //3 kat, 2 salon, 4 koltuk var
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "X";
+            textBox1.BackColor = Color.Red;
+                              
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.BackColor = Color.Yellow;
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "X";
+            textBox2.BackColor = Color.Red;
+        }
+
+        private void textBox2_DoubleClick(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox2.BackColor = Color.Yellow;
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "X";
+            textBox3.BackColor = Color.Red;
+        }
+
+        private void textBox3_DoubleClick(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            textBox3.BackColor = Color.Yellow;
+        }
+
+        private void textBox4_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "X";
+            textBox4.BackColor = Color.Red;
+        }
+
+        private void textBox4_DoubleClick(object sender, EventArgs e)
+        {
+            textBox4.Text = "";
+            textBox4.BackColor = Color.Yellow;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //alınmışsa koltuklar dolu, boşsa koltuklar boş olacak
+
+            textBox1.Text = sinema[comboBox1.SelectedIndex, comboBox2.SelectedIndex, 0];
+            textBox2.Text = sinema[comboBox1.SelectedIndex, comboBox2.SelectedIndex, 1];
+            textBox3.Text = sinema[comboBox1.SelectedIndex, comboBox2.SelectedIndex, 2];
+            textBox4.Text = sinema[comboBox1.SelectedIndex, comboBox2.SelectedIndex, 3];
+
+            if (textBox1.Text == "X")
+                textBox1.BackColor = Color.Red;
+            else
+                textBox1.BackColor = Color.Yellow;
+
+            if (textBox2.Text == "X")
+                textBox2.BackColor = Color.Red;
+            else
+                textBox2.BackColor = Color.Yellow;
+
+            if (textBox3.Text == "X")
+                textBox3.BackColor = Color.Red;
+            else
+                textBox3.BackColor = Color.Yellow;
+
+            if (textBox4.Text == "X")
+                textBox4.BackColor = Color.Red;
+            else
+                textBox4.BackColor = Color.Yellow;
+
+            biletbtn.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //hiçbir şey seçili değilken koltuklar sarı gelecek
+
+            if (comboBox1.SelectedIndex == -1)
+            {
+                textBox1.BackColor = Color.Yellow;
+                textBox2.BackColor = Color.Yellow;
+                textBox3.BackColor = Color.Yellow;
+                textBox4.BackColor = Color.Yellow;
+            }
+        }
+
+        private void biletbtn_Click(object sender, EventArgs e)
+        {
+            sinema[comboBox1.SelectedIndex, comboBox2.SelectedIndex, 0] = textBox1.Text;
+            sinema[comboBox1.SelectedIndex, comboBox2.SelectedIndex, 1] = textBox2.Text;
+            sinema[comboBox1.SelectedIndex, comboBox2.SelectedIndex, 2] = textBox3.Text;
+            sinema[comboBox1.SelectedIndex, comboBox2.SelectedIndex, 3] = textBox4.Text;
+
+            //comboBox1.ResetText();
+            //comboBox2.ResetText();
+
+            //comboBox1.SelectedIndex = -1;
+            //comboBox2.SelectedIndex = -1;
+
+            textBox1.BackColor = Color.Yellow;
+            textBox2.BackColor = Color.Yellow;
+            textBox3.BackColor = Color.Yellow;
+            textBox4.BackColor = Color.Yellow;
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+            biletbtn.Enabled = false;
+            MessageBox.Show("Bilet alınmıştır");
+        }
+
+        string satir = "";
+        string kat = ""; 
+        string salon = "";
+
+        private void listelebtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Kat" + "\t" + "Salon" + "\t" + "Koltuk1" + "\t" +"  Koltuk2" + "\t" + "Koltuk3" + "\t" + "Koltuk4" + "\t");
+            listBox1.Items.Add("--------------------------------------------------------------------------------------------------------------");
+
+
+            for (int i = 0; i < sinema.GetLength(0); i++)
+            {
+                for (int j = 0; j < sinema.GetLength(1); j++)
+                {
+                    for (int k = 0; k < sinema.GetLength(2); k++)
+                    {
+                        if (sinema[i, j, k] != "X")
+                        {
+                            sinema[i, j, k] = "B";
+                        }
+
+                        if (i == 0)
+                            kat = "A";
+                        else if (i == 1)
+                            kat = "B";
+                        else
+                            kat = "C";
+                           
+
+                        //listBox1.Items.Add(sinema[i, j, k]);
+
+                        int x = (j + 1);
+                        salon = x.ToString();
+                        satir += sinema[i, j, k] + "\t";
+
+
+                        //listBox1.Items.Add(i + j + k);
+
+                        //listBox1.Items.Add(sinema[i, j, k]);
+
+
+                        //if (sinema[i, j, k] == "X")
+                        //{
+                        //    listBox1.Items.Add((i + 1).ToString());
+                        //    listBox1.Items.Add((j + 1).ToString());
+                        //}
+
+                    }
+                    listBox1.Items.Add(kat + "\t" + salon + "\t" + satir);
+                    satir = "";
+                }
+                
+            }
+
+        }
+    }
+}
