7bd0d5457696daa46f5dad1330e623eac6dd8db9
seevdey <svdeyilmaz@gmail.com>
2022-12-03 07:06:31 +0300

    update

--- oop/07 - override-virtual-protected/oop7form0212/oop7form0212/Form1.Designer.cs
+++ oop/07 - override-virtual-protected/oop7form0212/oop7form0212/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
             this.adtxt = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -57,65 +57,72 @@
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.groupBox1.Location = new System.Drawing.Point(112, 23);
+            this.groupBox1.Location = new System.Drawing.Point(149, 28);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(326, 75);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(435, 92);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Personel Bilgileri";
             // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(36, 32);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(72, 21);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Çalışan";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(133, 32);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(66, 21);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Müdür";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
-            // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(224, 32);
+            this.radioButton3.Location = new System.Drawing.Point(299, 39);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(67, 21);
+            this.radioButton3.Size = new System.Drawing.Size(81, 24);
             this.radioButton3.TabIndex = 2;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Satışçı";
             this.radioButton3.UseVisualStyleBackColor = true;
             this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(177, 39);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(77, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Müdür";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(48, 39);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(86, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Çalışan";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.PeachPuff;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(203, 121);
+            this.label1.Location = new System.Drawing.Point(271, 149);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 18);
+            this.label1.Size = new System.Drawing.Size(53, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "Adı : ";
             // 
             // adtxt
             // 
-            this.adtxt.Location = new System.Drawing.Point(260, 122);
+            this.adtxt.Location = new System.Drawing.Point(347, 150);
+            this.adtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.adtxt.Name = "adtxt";
-            this.adtxt.Size = new System.Drawing.Size(100, 20);
+            this.adtxt.Size = new System.Drawing.Size(132, 22);
             this.adtxt.TabIndex = 2;
             // 
             // label2
@@ -123,9 +130,10 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.PeachPuff;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(177, 153);
+            this.label2.Location = new System.Drawing.Point(236, 188);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 18);
+            this.label2.Size = new System.Drawing.Size(80, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "Ssk No :";
             // 
@@ -134,9 +142,10 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.PeachPuff;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(186, 185);
+            this.label3.Location = new System.Drawing.Point(248, 228);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 18);
+            this.label3.Size = new System.Drawing.Size(70, 24);
             this.label3.TabIndex = 4;
             this.label3.Text = "Maaş : ";
             // 
@@ -145,9 +154,10 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.PeachPuff;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(145, 217);
+            this.label4.Location = new System.Drawing.Point(193, 267);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(98, 18);
+            this.label4.Size = new System.Drawing.Size(121, 24);
             this.label4.TabIndex = 5;
             this.label4.Text = "Zam Miktarı : ";
             // 
@@ -156,9 +166,10 @@
             this.satislbl.AutoSize = true;
             this.satislbl.BackColor = System.Drawing.Color.PeachPuff;
             this.satislbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.satislbl.Location = new System.Drawing.Point(147, 249);
+            this.satislbl.Location = new System.Drawing.Point(196, 306);
+            this.satislbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.satislbl.Name = "satislbl";
-            this.satislbl.Size = new System.Drawing.Size(96, 18);
+            this.satislbl.Size = new System.Drawing.Size(117, 24);
             this.satislbl.TabIndex = 6;
             this.satislbl.Text = "Satış Sayısı : ";
             this.satislbl.Visible = false;
@@ -168,47 +179,53 @@
             this.karlbl.AutoSize = true;
             this.karlbl.BackColor = System.Drawing.Color.PeachPuff;
             this.karlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.karlbl.Location = new System.Drawing.Point(127, 281);
+            this.karlbl.Location = new System.Drawing.Point(169, 308);
+            this.karlbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.karlbl.Name = "karlbl";
-            this.karlbl.Size = new System.Drawing.Size(116, 18);
+            this.karlbl.Size = new System.Drawing.Size(145, 24);
             this.karlbl.TabIndex = 7;
             this.karlbl.Text = "Departman Kar: ";
             this.karlbl.Visible = false;
             // 
             // ssktxt
             // 
-            this.ssktxt.Location = new System.Drawing.Point(260, 154);
+            this.ssktxt.Location = new System.Drawing.Point(347, 190);
+            this.ssktxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ssktxt.Name = "ssktxt";
-            this.ssktxt.Size = new System.Drawing.Size(100, 20);
+            this.ssktxt.Size = new System.Drawing.Size(132, 22);
             this.ssktxt.TabIndex = 8;
             // 
             // maastxt
             // 
-            this.maastxt.Location = new System.Drawing.Point(260, 186);
+            this.maastxt.Location = new System.Drawing.Point(347, 229);
+            this.maastxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.maastxt.Name = "maastxt";
-            this.maastxt.Size = new System.Drawing.Size(100, 20);
+            this.maastxt.Size = new System.Drawing.Size(132, 22);
             this.maastxt.TabIndex = 9;
             // 
             // zamtxt
             // 
-            this.zamtxt.Location = new System.Drawing.Point(260, 218);
+            this.zamtxt.Location = new System.Drawing.Point(347, 268);
+            this.zamtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.zamtxt.Name = "zamtxt";
-            this.zamtxt.Size = new System.Drawing.Size(100, 20);
+            this.zamtxt.Size = new System.Drawing.Size(132, 22);
             this.zamtxt.TabIndex = 10;
             // 
             // satistxt
             // 
-            this.satistxt.Location = new System.Drawing.Point(260, 250);
+            this.satistxt.Location = new System.Drawing.Point(347, 308);
+            this.satistxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.satistxt.Name = "satistxt";
-            this.satistxt.Size = new System.Drawing.Size(100, 20);
+            this.satistxt.Size = new System.Drawing.Size(132, 22);
             this.satistxt.TabIndex = 11;
             this.satistxt.Visible = false;
             // 
             // kartxt
             // 
-            this.kartxt.Location = new System.Drawing.Point(260, 282);
+            this.kartxt.Location = new System.Drawing.Point(347, 310);
+            this.kartxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.kartxt.Name = "kartxt";
-            this.kartxt.Size = new System.Drawing.Size(100, 20);
+            this.kartxt.Size = new System.Drawing.Size(132, 22);
             this.kartxt.TabIndex = 12;
             this.kartxt.Visible = false;
             // 
@@ -216,9 +233,10 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Tan;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(216, 328);
+            this.button1.Location = new System.Drawing.Point(288, 364);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(85, 37);
+            this.button1.Size = new System.Drawing.Size(113, 46);
             this.button1.TabIndex = 13;
             this.button1.Text = "EKLE";
             this.button1.UseVisualStyleBackColor = false;
@@ -226,10 +244,13 @@
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(112, 426);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(149, 467);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(340, 199);
+            this.listBox1.Size = new System.Drawing.Size(443, 164);
             this.listBox1.TabIndex = 14;
             // 
             // label7
@@ -238,18 +259,19 @@
             this.label7.BackColor = System.Drawing.Color.Maroon;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(109, 396);
+            this.label7.Location = new System.Drawing.Point(145, 430);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(120, 20);
+            this.label7.Size = new System.Drawing.Size(149, 25);
             this.label7.TabIndex = 15;
             this.label7.Text = "Personel Listesi";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RosyBrown;
-            this.ClientSize = new System.Drawing.Size(553, 648);
+            this.ClientSize = new System.Drawing.Size(737, 798);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
@@ -267,6 +289,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Çalışan";
--- oop/07 - override-virtual-protected/oop7form0212/oop7form0212/Form1.cs
+++ oop/07 - override-virtual-protected/oop7form0212/oop7form0212/Form1.cs
@@ -17,7 +17,6 @@ namespace oop7form0212
             InitializeComponent();
         }
 
-
         class Calisan : IDisposable
         {
             private string _ad;
@@ -44,17 +43,32 @@ namespace oop7form0212
                 set { _maas = value; }
             }
 
-            public virtual void zamYap(double zam) 
+            public virtual void zamYap(double zam)
             {
                 //Bu sınıftan kalıtım yoluyla oluşturulan sınıflar tarafından kullananılabilir bir metot olması için virtual kullanıyoruz
                 _maas = _maas + zam;
             }
 
-            public virtual void BilgiVer(string ozellik)
+            public virtual string BilgiVer(string ozellik)
             {
+                if (ozellik is null)
+                {
+                    return ADI + " / " + SSKNO + " / " + MAAS;
 
+                }
+
+                switch (ozellik)
+                {
+                    case "ADI":
+                        return ADI;
+                    case "MAAS":
+                        return MAAS.ToString();
+                    case "SSKNO":
+                        return SSKNO.ToString();
+                    default:
+                        return "boş";
+                }
             }
-
 
             public void Dispose()
             {
@@ -64,9 +78,9 @@ namespace oop7form0212
 
         class Mudur : Calisan //Mudur class ı Calisan 
         {
-            private int departmanKar;
+            private double departmanKar;
 
-            public int DEPARTMANKAR
+            public double DEPARTMANKAR
             {
                 get { return departmanKar; }
                 set { departmanKar = value; }
@@ -74,29 +88,24 @@ namespace oop7form0212
 
             public override void zamYap(double zam)
             {
-                double prim = 0.0;
+                double prim = 0;
 
                 if (MAAS > 0)
                 {
                     if (departmanKar > 0 && departmanKar < 100)
-                    {
                         prim = 1000;
-                    }
                     else if (departmanKar > 100 && departmanKar < 1000)
-                    {
                         prim = 2000;
-                    }
                     else
-                    {
                         prim = 3000;
-                    }
-                    MAAS += zam + prim;
                 }
 
                 else
                 {
-                    return;
+                    //MessageBox.Show("Maaş Bilgisi Yoktur");
+                    //return;
                 }
+                MAAS += zam + prim;
 
             }
 
@@ -119,20 +128,25 @@ namespace oop7form0212
                 if (MAAS > 0)
                 {
                     if (satisSayisi > 0 && satisSayisi < 100)
-                    {
                         prim = 100;
-                    }
+                    else if (satisSayisi >= 100 && satisSayisi < 300)
+                        prim = 500;
                     else
-                    {
+                        prim = 1000;
 
-                    }
                 }
-                
+                else
+                {
+                    //MessageBox.Show("Maaş Bilgisi Yoktur");
+                    //return;
+                }
+                MAAS += zam + prim;
+
             }
 
-            public override void BilgiVer(string ozellik)
+            public override string BilgiVer(string ozellik)
             {
-
+                return base.BilgiVer(ozellik) + " / " + satisSayisi;
             }
 
         }
@@ -140,7 +154,63 @@ namespace oop7form0212
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (radioButton1.Checked)
+                {
+                    using (Calisan cc = new Calisan())
+                    {
+                        cc.ADI = adtxt.Text;
+                        cc.SSKNO = int.Parse(ssktxt.Text);
+                        cc.MAAS = int.Parse(maastxt.Text);
+                        cc.zamYap(Convert.ToDouble(zamtxt.Text));
+                        listBox1.Items.Add($"Çalışan Bilgileri : \n{cc.BilgiVer(null)}");
+                    }
+                }
 
+                else if (radioButton2.Checked)
+                {
+                    using (Mudur mm = new Mudur())
+                    {
+                        mm.ADI = adtxt.Text;
+                        mm.SSKNO = int.Parse(ssktxt.Text);
+                        mm.MAAS = int.Parse(maastxt.Text);
+                        mm.DEPARTMANKAR = Convert.ToDouble(kartxt.Text);
+                        mm.zamYap(Convert.ToDouble(zamtxt.Text));
+                        listBox1.Items.Add($"Müdür Bilgileri : \n{mm.BilgiVer("ADI")}");
+                    }
+                }
+
+                else{
+                    using (SatisElemani ss = new SatisElemani())
+                    {
+                        ss.ADI = adtxt.Text;
+                        ss.SSKNO = int.Parse(ssktxt.Text);
+                        ss.MAAS = int.Parse(maastxt.Text);
+                        ss.SATISSAYISI = Convert.ToInt32(satistxt.Text);
+                        ss.zamYap(Convert.ToDouble(zamtxt.Text));
+                        listBox1.Items.Add($"Satış Elemanı : \n{ss.BilgiVer(null)}");
+                    }
+                }
+
+                
+
+                
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void Temizle()
+        {
+            adtxt.Clear();
+            ssktxt.Clear();
+            maastxt.Clear();
+            zamtxt.Clear();
+            satistxt.Clear();
+            //listBox1.Items.Clear();
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
@@ -149,12 +219,17 @@ namespace oop7form0212
             satistxt.Visible = false;
             karlbl.Visible = false;
             kartxt.Visible = false;
+            Temizle();
+
         }
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
             kartxt.Visible = true;
             karlbl.Visible = true;
+            Temizle();
+
+
         }
 
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
@@ -163,6 +238,10 @@ namespace oop7form0212
             satistxt.Visible = true;
             karlbl.Visible = false;
             kartxt.Visible = false;
+            Temizle();
+
+
+
         }
     }
 }
