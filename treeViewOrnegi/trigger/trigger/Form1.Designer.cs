40175f46777883147f859e4186d8545653f8682a
seevdey <svdeyilmaz@gmail.com>
2023-01-01 09:54:41 +0300

    treeView - 30.12

---
+++ treeViewOrnegi/trigger/trigger/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace trigger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(237, 563);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(238, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(619, 539);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Ürün Adı";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle4.Format = "N2";
+            dataGridViewCellStyle4.NullValue = null;
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Column2.HeaderText = "Fiyatı";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Stok Miktarı";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 563);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ treeViewOrnegi/trigger/trigger/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trigger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string baglantiCumlesi = "Data Source=SEVDE;Initial Catalog=Northwind;Integrated Security=True";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string komut = "";
+                komut = "select * from Categories";
+                treeView1.Nodes.Clear();
+                treeView1.Nodes.Add("Kategoriler");
+
+                using (SqlConnection baglanti = new SqlConnection())
+                {
+                    baglanti.ConnectionString = baglantiCumlesi;
+                    using (SqlCommand listelemekomut = new SqlCommand(komut, baglanti))
+                    {
+                        baglanti.Open();
+                        using (SqlDataReader okuyucu = listelemekomut.ExecuteReader())
+                        {
+                            while (okuyucu.Read())
+                            {
+                                treeView1.Nodes[0].Nodes.Add(okuyucu[0].ToString() + "-" + okuyucu[1].ToString());
+                            }
+                        }
+                        baglanti.Close();
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //metoda al, parametreye göre çağır
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string str = treeView1.SelectedNode.Text; //seçildiği zaman str ye al
+            dataGridView1.Rows.Clear();
+            if (str == "Kategoriler") //ilk açıldığında kategorilere göre çalıştır
+            {
+                try
+                {
+                    string komut = "";
+                    komut = "select ProductName, UnitPrice, UnitsInStock from Products";
+                    using (SqlConnection baglanti = new SqlConnection())
+                    {
+                        baglanti.ConnectionString = baglantiCumlesi;
+                        using (SqlCommand listelemekomut = new SqlCommand(komut, baglanti))
+                        {
+                            baglanti.Open();
+                            using (SqlDataReader okuyucu = listelemekomut.ExecuteReader())
+                            {
+                                while (okuyucu.Read())
+                                {
+                                    dataGridView1.Rows.Add(okuyucu[0], okuyucu[1], okuyucu[2]);
+
+                                }
+                            }
+                            baglanti.Close();
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else //seçilmiş noda göre çalıştır
+            {
+                string code = str.Split('-')[0];
+                try
+                {
+                    string komut = "";
+                    //satır bazında ekleme yaptık
+                    komut = "select ProductName, UnitPrice, UnitsInStock from Products where CategoryID = '" + code + "'";
+                    using (SqlConnection baglanti = new SqlConnection())
+                    {
+                        baglanti.ConnectionString = baglantiCumlesi;
+                        using (SqlCommand listelemekomut = new SqlCommand(komut, baglanti))
+                        {
+                            baglanti.Open();
+                            using (SqlDataReader okuyucu = listelemekomut.ExecuteReader())
+                            {
+                                while (okuyucu.Read())
+                                {
+                                    //okuyucu
+                                    dataGridView1.Rows.Add(okuyucu[0], okuyucu[1], okuyucu[2]);
+
+                                }
+                            }
+                            baglanti.Close();
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+    }
+}
